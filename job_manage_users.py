706d3d2bedfd20fd9045f8aa6e74b158ecddea2b
Jose76Tron <songoanda59@hotmail.es>
2023-08-22 19:01:05 +0100

    - Better formatting - Dont delete Global Admin users from Azure AD

--- job_manage_users.py
+++ job_manage_users.py
@@ -4,20 +4,26 @@ import string
 from helpers import *
 
 
-class ManageUsers():
+class ManageUsers:
     def __init__(self, access_token=""):
         self.access_token = access_token
-    
+
     # Get current user count
     def get_user_count(self):
-        response = call_api_get("https://graph.microsoft.com/v1.0/users", self.access_token)
+        response = call_api_get(
+            "https://graph.microsoft.com/v1.0/users", self.access_token
+        )
         return len(response.json()["value"])
-    
+
     # Get a random user (userPrincipalName)
     def get_random_user(self):
-        response = call_api_get("https://graph.microsoft.com/v1.0/users", self.access_token)
-        return random.choice([item.get("userPrincipalName") for item in response.json()["value"]])
-    
+        response = call_api_get(
+            "https://graph.microsoft.com/v1.0/users", self.access_token
+        )
+        return random.choice(
+            [item.get("userPrincipalName") for item in response.json()["value"]]
+        )
+
     # Add a user with auto generated user's info
     def add_user(self, recursive_count=0):
         user = __class__.generate_random_name()
@@ -34,42 +40,76 @@ class ManageUsers():
             "preferredLanguage": "en",
             "passwordProfile": {
                 "forceChangePasswordNextSignIn": True,
-                "password": __class__.generate_password()
-            }
+                "password": __class__.generate_password(),
+            },
         }
-        response = call_api_post("https://graph.microsoft.com/v1.0/users", data, self.access_token)
+        response = call_api_post(
+            "https://graph.microsoft.com/v1.0/users", data, self.access_token
+        )
         if response.status_code == 201:
             print("{}: {} {}".format("Success", "Add user", data["userPrincipalName"]))
         else:
-            print("{}: {} {} {} {}".format("Error", "Add user", data["userPrincipalName"], response.status_code, response.text))
+            print(
+                "{}: {} {} {} {}".format(
+                    "Error",
+                    "Add user",
+                    data["userPrincipalName"],
+                    response.status_code,
+                    response.text,
+                )
+            )
             if recursive_count < 5:
-                self.add_user(recursive_count + 1) # Recursively call this function until its success
-    
+                self.add_user(
+                    recursive_count + 1
+                )  # Recursively call this function until its success
+
     # Delete a user by id or userPrincipalName
-    def delete_user_by_id(self, id):
-        response = call_api_delete(f"https://graph.microsoft.com/v1.0/users/{id}", self.access_token)
+    def delete_user_by_id(self, id, recursive_count=0):
+        response = call_api_delete(
+            f"https://graph.microsoft.com/v1.0/users/{id}", self.access_token
+        )
         if response.status_code == 204:
             print("{}: {} {}".format("Success", "Delete user", id))
         else:
-            print("{}: {} {} {} {}".format("Error", "Delete user", id, response.status_code, response.text))
-    
-    # Delete a user randomly
-    def delete_a_random_user(self, recursive_count=0):
-        user_id = self.get_random_user()
-        if "Jose76Tron".lower() not in user_id.lower():
-            # if not self.check_user_role(user_id):
-            #     self.delete_user_by_id(user_id)
-            # else:
-            print("{}: {} {} {}".format("Error", "Delete user", user_id, "Cannot delete the Global Admin"))
+            print(
+                "{}: {} {} {} {}".format(
+                    "Error", "Delete user", id, response.status_code, response.text
+                )
+            )
             if recursive_count < 5:
-                self.delete_a_random_user(recursive_count + 1) # Recursively call this function until its success
-    
+                self.delete_a_random_user(
+                    self, id, recursive_count + 1
+                )  # Recursively call this function until its success to delete the user
+
+    # Delete a user randomly
+    def delete_a_random_user(self):
+        user_id = self.get_random_user()
+        if not self.check_user_role(user_id):
+            self.delete_user_by_id(user_id)
+        else:
+            print(
+                "{}: {} {} {}".format(
+                    "Error", "Delete user", user_id, "Cannot delete the Global Admin"
+                )
+            )
+
     # Check if user is an Admin
-    def check_user_role(self, id):
-        response = call_api_get(f"https://graph.microsoft.com/v1.0/users/{id}/memberOf", self.access_token)
+    def check_user_role(id):
+        response = call_api_get(
+            f"https://graph.microsoft.com/v1.0/users/{id}/memberOf", id
+        )
+
+        # if response status code is not 200, return False
+        if response.status_code != 200:
+            return False
+
+        # if json value not exists, return False
+        if not response.json().get("value"):
+            return False
+
         is_admin = any(
-            item.get("@odata.type") == "#microsoft.graph.directoryRole" and
-            item.get("roleTemplateId") == "62e90394-69f5-4237-9190-012177145e10"
+            item.get("@odata.type") == "#microsoft.graph.directoryRole"
+            and item.get("roleTemplateId") == "62e90394-69f5-4237-9190-012177145e10"
             for item in response.json()["value"]
         )
         return is_admin
@@ -82,8 +122,70 @@ class ManageUsers():
 
     @staticmethod
     def generate_random_name():
-        first_names = ["James", "Robert", "John", "Michael", "David", "William", "Richard", "Joseph", "Thomas", "Christopher", "Charles", "Daniel", "Matthew", "Anthony", "Mark", "Mary", "Patricia", "Jennifer", "Linda", "Elizabeth", "Barbara", "Susan", "Jessica", "Sarah", "Karen", "Lisa", "Nancy", "Betty", "Sandra", "Margaret"]
-        last_names = ["Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez", "Hernandez", "Lopez", "Gonzales", "Wilson", "Anderson", "Thomas", "Taylor", "Moore", "Jackson", "Martin", "Lee", "Perez", "Thompson", "White", "Harris", "Sanchez", "Clark", "Ramirez", "Lewis", "Robinson"]
+        first_names = [
+            "James",
+            "Robert",
+            "John",
+            "Michael",
+            "David",
+            "William",
+            "Richard",
+            "Joseph",
+            "Thomas",
+            "Christopher",
+            "Charles",
+            "Daniel",
+            "Matthew",
+            "Anthony",
+            "Mark",
+            "Mary",
+            "Patricia",
+            "Jennifer",
+            "Linda",
+            "Elizabeth",
+            "Barbara",
+            "Susan",
+            "Jessica",
+            "Sarah",
+            "Karen",
+            "Lisa",
+            "Nancy",
+            "Betty",
+            "Sandra",
+            "Margaret",
+        ]
+        last_names = [
+            "Smith",
+            "Johnson",
+            "Williams",
+            "Brown",
+            "Jones",
+            "Garcia",
+            "Miller",
+            "Davis",
+            "Rodriguez",
+            "Martinez",
+            "Hernandez",
+            "Lopez",
+            "Gonzales",
+            "Wilson",
+            "Anderson",
+            "Thomas",
+            "Taylor",
+            "Moore",
+            "Jackson",
+            "Martin",
+            "Lee",
+            "Perez",
+            "Thompson",
+            "White",
+            "Harris",
+            "Sanchez",
+            "Clark",
+            "Ramirez",
+            "Lewis",
+            "Robinson",
+        ]
         first_name = random.choice(first_names)
         last_name = random.choice(last_names)
         return {
@@ -91,18 +193,20 @@ class ManageUsers():
             "givenName": f"{first_name}",
             "surname": f"{last_name}",
             "mailNickname": f"{first_name}{last_name[:1]}",
-            "userPrincipalName": f"{first_name}{last_name[:1]}".lower()
+            "userPrincipalName": f"{first_name}{last_name[:1]}".lower(),
         }
 
     @staticmethod
     def generate_random_phone_number():
         first = "0" + random.choice(["3", "5", "8", "9"])
         second = str(random.randint(0, 999)).zfill(3)
-        last = (str(random.randint(0, 9999)).zfill(4))
+        last = str(random.randint(0, 9999)).zfill(4)
         return "{}{}{}".format(first, second, last)
 
     def get_random_domain(self):
-        response = call_api_get("https://graph.microsoft.com/v1.0/domains", self.access_token)
+        response = call_api_get(
+            "https://graph.microsoft.com/v1.0/domains", self.access_token
+        )
         return random.choice([item.get("id") for item in response.json()["value"]])
 
     # Main run
@@ -112,7 +216,10 @@ class ManageUsers():
         plan_to_delete = random.randint(1, 10)
         user_count_midpoint = 10
         target_user_count = current_user_count + plan_to_add - plan_to_delete
-        if target_user_count >= 0.5 * user_count_midpoint and target_user_count <= 1.5 * user_count_midpoint:
+        if (
+            target_user_count >= 0.5 * user_count_midpoint
+            and target_user_count <= 1.5 * user_count_midpoint
+        ):
             print("Pre-process user count: {} users".format(current_user_count))
             print("Plan to add: {} users".format(plan_to_add))
             print("Plan to delete: {} users".format(plan_to_delete))
@@ -123,7 +230,7 @@ class ManageUsers():
                 self.delete_a_random_user()
             print("Post-process user count: {} users".format(self.get_user_count()))
         else:
-            self.run() # Recursively call this function until it meets the condition
+            self.run()  # Recursively call this function until it meets the condition
 
 
 if __name__ == "__main__":
--- ms_tokens.py
+++ ms_tokens.py
@@ -7,13 +7,27 @@ import shutil
 from nacl import encoding, public
 
 
-class MSTokens():
-    def __init__(self, mode="pro", client_id=None, client_secret=None, refresh_token=None, github_token=None, github_repo=None, tokens_file_name=None):
+class MSTokens:
+    def __init__(
+        self,
+        mode="pro",
+        client_id=None,
+        client_secret=None,
+        refresh_token=None,
+        github_token=None,
+        github_repo=None,
+        tokens_file_name=None,
+    ):
         self.app_folder = os.path.dirname(os.path.realpath(__file__))
         self.mode = mode
         self.client_id = client_id
         self.client_secret = client_secret
-        if mode == "pro" and refresh_token is not None and github_token is not None and github_repo is not None:
+        if (
+            mode == "pro"
+            and refresh_token is not None
+            and github_token is not None
+            and github_repo is not None
+        ):
             self.refresh_token = refresh_token
             self.github_token = github_token
             self.github_repo = github_repo
@@ -28,15 +42,15 @@ class MSTokens():
     # Get the public key of the Github repo
     def get_public_key(self):
         url = f"https://api.github.com/repos/{self.github_repo}/actions/secrets/public-key"
-        headers = {
-            "Authorization": f"token {self.github_token}"
-        }
+        headers = {"Authorization": f"token {self.github_token}"}
         response = requests.get(url, headers=headers)
-        return response.json() # Including key and key_id
+        return response.json()  # Including key and key_id
 
     # Encrypt the secret value using the public key
     def encrypt(self, public_key, secret_value):
-        public_key = public.PublicKey(public_key.encode("utf-8"), encoding.Base64Encoder())
+        public_key = public.PublicKey(
+            public_key.encode("utf-8"), encoding.Base64Encoder()
+        )
         sealed_box = public.SealedBox(public_key)
         encrypted = sealed_box.encrypt(secret_value.encode("utf-8"))
         return encoding.Base64Encoder().encode(encrypted).decode("utf-8")
@@ -44,32 +58,36 @@ class MSTokens():
     # Update the secret
     def update_secret(self, secret_name, secret_value):
         url = f"https://api.github.com/repos/{self.github_repo}/actions/secrets/{secret_name}"
-        headers = {
-            "Authorization": f"token {self.github_token}"
-        }
+        headers = {"Authorization": f"token {self.github_token}"}
         public_key = self.get_public_key()
         data = {
             "encrypted_value": self.encrypt(public_key["key"], secret_value),
-            "key_id": public_key["key_id"]
+            "key_id": public_key["key_id"],
         }
         response = requests.put(url, headers=headers, json=data)
         if response.status_code == 204:
             print("{}: {} {}".format("Success", "Update GitHub secret", secret_name))
         else:
-            print("{}: {} {} {} {}".format("Error", "Update GitHub secret", secret_name, response.status_code, response.text))
+            print(
+                "{}: {} {} {} {}".format(
+                    "Error",
+                    "Update GitHub secret",
+                    secret_name,
+                    response.status_code,
+                    response.text,
+                )
+            )
 
     # Get a new access token and refresh token
     def get_access_token(self):
         url = "https://login.microsoftonline.com/common/oauth2/v2.0/token"
-        headers = {
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
         data = {
             "grant_type": "refresh_token",
             "refresh_token": self.refresh_token,
             "client_id": self.client_id,
             "client_secret": self.client_secret,
-            "redirect_uri": "http://localhost:53682/"
+            "redirect_uri": "http://localhost:53682/",
         }
         response = requests.post(url, data=data, headers=headers)
         if response.status_code == 200:
@@ -77,21 +95,30 @@ class MSTokens():
             if self.mode == "pro":
                 self.update_secret("REFRESH_TOKEN", response.json()["refresh_token"])
             elif self.mode == "dev":
-                self.save_tokens_to_file(json.dumps(response.json())) # Save data into file
+                self.save_tokens_to_file(
+                    json.dumps(response.json())
+                )  # Save data into file
             return response.json()["access_token"]
         else:
-            print("{}: {} {} {}".format("Error", "Get access token", response.status_code, response.text))
+            print(
+                "{}: {} {} {}".format(
+                    "Error", "Get access token", response.status_code, response.text
+                )
+            )
 
     # Get refresh token from file
     def get_refresh_token_from_file(self):
         with open(self.tokens_file_path, "r") as file:
             data = file.read()
         return json.loads(data)["refresh_token"]
-    
+
     # Save the new access token and refresh token into file
     def save_tokens_to_file(self, data):
         # Create a backup
-        tokens_backup_file_path = os.path.join(self.app_folder, self.tokens_file_name[:-4] + "-" + str(int(time.time())) + ".txt")
+        tokens_backup_file_path = os.path.join(
+            self.app_folder,
+            self.tokens_file_name[:-4] + "-" + str(int(time.time())) + ".txt",
+        )
         shutil.copy2(self.tokens_file_path, tokens_backup_file_path)
         # Try to write new contents into file
         try:
@@ -112,7 +139,7 @@ if __name__ == "__main__":
         client_secret=sys.argv[2],
         refresh_token=sys.argv[3],
         github_token=sys.argv[4],
-        github_repo=sys.argv[5]
+        github_repo=sys.argv[5],
     )
     print(mst.get_access_token())
 
